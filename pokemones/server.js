7f00c9f47615be97e59e74c41ae03d5ef7b0b93a
YanMachadoCarrillo <yanca.next@gmail.com>
2026-05-19 08:51:39 -0500

    Update server.js

--- pokemones/server.js
+++ pokemones/server.js
@@ -4,7 +4,7 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // MYSQL CLOUD AIVEN
 const pool = mysql.createPool({
