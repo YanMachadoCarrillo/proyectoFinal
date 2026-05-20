2070342a227d7169e5fb4b8a4add005c51945ae7
YanMachadoCarrillo <yanca.next@gmail.com>
2026-05-19 21:44:46 -0500

    Update NarutoController.java

--- naruto/src/main/java/com/api/controller/NarutoController.java
+++ naruto/src/main/java/com/api/controller/NarutoController.java
@@ -1,1 +1,28 @@
+package com.api.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/naruto")
+@CrossOrigin("*")
+public class NarutoController {
+
+    @GetMapping
+    public List<Map<String, Object>> getAll() {
+
+        return List.of(
+
+            Map.of(
+                "id", 1,
+                "nombre", "Naruto Uzumaki",
+                "aldea", "Konoha",
+                "rango", "Hokage",
+                "imagen",
+                "https://i.imgur.com/2yaf2wb.png"
+            )
+        );
+    }
+}
